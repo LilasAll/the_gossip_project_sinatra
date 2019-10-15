4c618f8f578556414366362013b0828083c46f86
LilasAll <lilas.allard@gmail.com>
2019-10-15 13:57:08 +0200

    finish 1st exercices

--- app.rb
+++ app.rb
@@ -11,4 +11,36 @@ require_relative 'lib/player'
 # Maintenant c'est open bar pour tester ton application. Tous les fichiers importants sont chargés
 # Tu peux faire User.new, Event.new, binding.pry, User.all, etc.
 
-binding.pry
+	##Get user names
+	player1 = Player.new("Josiane")
+	player2 = Player.new("José")
+
+	while player1.lifepoints >= 0 or player2.lifepoints >= 0
+		##Get the life level and name of each player
+		puts "Voici l'état de chaque joueur :"
+		puts "#{player1.show_state} #{player2.show_state}"
+		puts "Passons à la phase d'attaque.. 3..2..1.. GOOOOOOOO"
+		
+		#Player 1 attacks player 2
+		player1.attacks(player2)
+
+			if player2.lifepoints <= 0
+				#If player 2 don't have more points = stop the game
+				break
+			else
+				#If player 2 still have some points	
+				player2.attacks(player1)
+					#If player1 have no more point = stop the game
+					if player1.lifepoints <= 0
+						break
+					#Else continue
+					else
+					end
+			end
+
+	end
+
+#binding.pry 
+
+
+
--- lib/player.rb
+++ lib/player.rb
@@ -17,12 +17,26 @@ class Player
 	def gets_damage(damages)
 		#substract the damage of the total lifepoints
 		@lifepoints = @lifepoints - damages
-
 		#Wanna know if the player still have lifepoints
 		if @lifepoints <= 0
 			puts "Le joueur #{@name} a été tué!!!!"
 		else
 		end
+	end
+
+	def attacks(player)
+		puts "Le joueur #{@name} attaque le joueur #{player.name}"
+		#Associate the value of damage to damage from compute_damage
+		damages = compute_damage
+		puts "Il inflige #{damages} points de dommages. "
+		#the player get damages from the other player
+		player.gets_damage(damages)
+
+	end
+
+	def compute_damage
+		#choose a random value of damage 
+		return rand(1..6)
 	end
 
 end
