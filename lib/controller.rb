6b4529071f868ef084fb97e082cc681169c71b5d
LilasAll <lilas.allard@gmail.com>
2019-10-21 12:24:48 +0200

    Adding files

--- lib/controller.rb
+++ lib/controller.rb
@@ -4,16 +4,27 @@ require_relative 'gossip'
 #Elle aura toutes les fonctionnalités de base de Sinatra 
 class ApplicationController < Sinatra::Base
 	#Si qql av sur l'URL / fait le code qui suit
-  get '/' do
-    erb :index
-  end
+
+	get '/' do
+	  erb :index, locals: {gossip: Gossip.all}
+	end
 
   get '/gossips/new/' do
   	erb :new_gossip
   end 
 
   post '/gossips/new/' do
-		Gossip.new("super_auteur", "super gossip").save
+		Gossip.new(params["gossip_author"], params["gossip_content"]).save
+		redirect '/'
 	end
+
+
+	#post '/gossips/new/' do
+	#  puts "Salut, je suis dans le serveur"
+	# puts "Ceci est le contenu du hash params : #{params}"
+	#puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
+	#  puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
+	#  puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
+	#end
 
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -14,8 +14,20 @@ class Gossip
 		#Ouvrir le csv en mode écriture ab, qui a pour chemin "./db..."
 		CSV.open("./db/gossip.csv", "ab") do |csv|
 			#Inserer une ligne avec deux colonne
-			csv << [author, content]
+			csv << [@author, @content]
 		end
+	end
+
+	def self.all
+		#initialise un array vide
+		all_gossips = [] 
+
+		CSV.read("./db/gossip.csv").each do |csv_line|
+			all_gossips << Gossip.new(csv_line[0], csv_line[1])	
+		end
+
+		return all_gossips
+
 	end
 
 end
